0281506d3870ac4d7f3a83c7289cde80cc3b5ee2
Faazuthnar <faazuthnar@faazuthnar.xyz>
2018-02-15 21:04:05 +0100

    zsh: Title manipulation, man colors

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -4,6 +4,58 @@ autoload -U colors && colors
 
 setopt PROMPT_SUBST
 
+# determine the window title escape sequences: https://unix.stackexchange.com/questions/7380/force-title-on-gnu-screen
+case "$TERM" in
+aixterm|dtterm|putty|rxvt|xterm*)
+	titlestart='\033]0;'
+	titlefinish='\007'
+    ;;
+cygwin)
+    titlestart='\033];'
+    titlefinish='\007'
+    ;;
+konsole)
+    titlestart='\033]30;'
+    titlefinish='\007'
+    ;;
+screen*)
+    # status line
+    #titlestart='\033_'
+    #titlefinish='\033\'
+    # window title
+    titlestart='\033k'
+    titlefinish='\033\'
+    ;;
+*)
+    if type tput >/dev/null 2>&1
+    then
+        if tput longname >/dev/null 2>&1
+        then
+            titlestart="$(tput tsl)"
+            titlefinish="$(tput fsl)"
+        fi
+    else
+        titlestart=''
+        titlefinish=''
+    fi
+    ;;
+esac
+
+# set the xterm/screen/etc. title
+settitle()
+{
+	test -z "${titlestart}" && return 0
+	printf "${titlestart}$*${titlefinish}"
+}
+
+TITLE=""
+if [ "$SSH_CLIENT" != "" ]; then # Add a useful prefix if in an ssh session
+	TITLE+="SSH: "
+fi
+TITLE+="${USER}@${HOST}" # Initial title
+export TITLE
+
+
 set_prompt() {
 
 	# [
@@ -49,6 +101,8 @@ set_prompt() {
 	PS1+='
 '
 	PS1+=": %{$reset_color%}% "
+
+	settitle "$TITLE"
 }
 
 precmd_functions+=set_prompt
@@ -56,6 +110,8 @@ precmd_functions+=set_prompt
 preexec () {
    (( ${#_elapsed[@]} > 1000 )) && _elapsed=(${_elapsed[@]: -1000})
    _start=$SECONDS
+
+   settitle "$TITLE: $1"
 }
 
 precmd () {
--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -35,6 +35,21 @@ source ~/dotfiles/zsh/plugins/fixls.zsh
 	}
 	alias cd="c"
 
+	# Man colors
+	manc() {
+		env \
+		LESS_TERMCAP_mb=$'\e[01;31m' \
+		LESS_TERMCAP_md=$'\e[01;31m' \
+		LESS_TERMCAP_me=$'\e[0m' \
+		LESS_TERMCAP_se=$'\e[0m' \
+		LESS_TERMCAP_so=$'\e[01;44;33m' \
+		LESS_TERMCAP_ue=$'\e[0m' \
+		LESS_TERMCAP_us=$'\e[01;32m' \
+		man "$*"
+	}
+	alias man="manc"
+
+
 # For vim mappings: 
 	stty -ixon
 
